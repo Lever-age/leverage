f7e8bb0c4197fc5b6fbf1297a3edde8b46604ecf
Bayo <adebayo.adejare@gmail.com>
2016-03-20 13:52:36 -0400

    ez_setup.py

--- DataAnalysis/data/data_clean.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-campaign2015 = pd.read_csv('2015ytd', values=('FilerName', 'Year', 'Cycle',	'DocType', 'EntityName',
-                                              'EntityAddressLine1', 'EntityAddressLine2',
-                                              'EntityCity', 'EntityState', 'EntityZip', 'Occupation', 'EmployerName',
-                                              'EmployerAddressLine1', 'EmployerAddressLine2', 'EmployerCity',
-                                              'EmployerState', 'EmployerZip', 'Date', 'Amount', 'Description',
-                                              'Amended', 'SubDate', 'FiledBy'))
